196d14bf73abfa513981d20e0329a3acd972f204
Luc4sQ <pq164536@uni-greifswald.de>
2023-07-03 15:27:19 +0200

    added some pseudocode

--- src/alg/cdcl.py
+++ src/alg/cdcl.py
@@ -1,1 +1,46 @@
-# test
+import numpy as np
+
+# test 
+
+def unitPropagation(cnf, ass):
+    pass
+
+def allVariablesAssigned():
+
+    return True
+
+def pickBranchingVariable():
+
+    pass
+
+def conflictAnalysis(cnf, ass):
+
+    pass
+
+def backtrack(cnf, ass, beta):
+
+    pass
+
+def cdcl(cnf, assignment):
+    if unitPropagation(cnf,assignment) == "conflict":
+        return "unsat"
+    dl = 0
+
+    while not allVariablesAssigned():
+        x = pickBranchingVariable()
+
+        dl += 1
+
+        assignment = assignment | set(x)
+
+        if unitPropagation(cnf, assignment) == "conflict":
+            beta = conflictAnalysis(cnf, assignment)
+
+            if beta < 0:
+                return "unsat"
+            else:
+                backtrack(cnf, assignment, beta)
+                dl = beta                
+    
+    
+    pass
